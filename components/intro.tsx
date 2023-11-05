8c06fe340e723a456e15dcc4d99224276e0dd521
Flavia Muntean <muntean.flavia@gmail.com>
2023-11-05 12:12:39 +0000

    Added links to contact, download CV, LinkedIn, and GitHub profiles

--- components/intro.tsx
+++ components/intro.tsx
@@ -3,6 +3,10 @@
 import Image from 'next/image';
 import React from 'react';
 import { motion } from 'framer-motion';
+import Link from 'next/link';
+import { BsArrowRight, BsLinkedin } from 'react-icons/bs';
+import { HiDownload } from 'react-icons/hi';
+import { FaGithubSquare } from 'react-icons/fa';
 
 const Intro = () => {
   return (
@@ -53,6 +57,54 @@ const Intro = () => {
         <span className="italic">sites & applications</span>. My focus is{' '}
         <span className="underline">React and React Native</span>.
       </motion.p>
+      <motion.div
+        className="flex flex-col sm:flex-row gap-4 items-center justify-center px-4 text-lg font-medium"
+        initial={{ opacity: 0, y: 100 }}
+        animate={{ opacity: 1, y: 0 }}
+        transition={{
+          delay: 0.1,
+        }}
+      >
+        <Link
+          href="#contact"
+          className="group bg-gray-900 text-white px-7 py-3 flex items-center gap-2 rounded-full 
+          outline-none focus:scale-110 hover:scale-110 hover:bg-gray-950 active:scale-105 transition-all"
+        >
+          Contact me here{' '}
+          <BsArrowRight className="opacity-70 group-hover:translate-x-1 transition" />
+        </Link>
+
+        <a
+          className="group bg-white px-7 py-3 flex items-center gap-2 rounded-full
+        outline-none focus:scale-110 hover:scale-110 active:scale-105 transition-all
+        cursor-pointer border border-black/10"
+          href="/CV.pdf"
+          download
+        >
+          Download CV{' '}
+          <HiDownload className="opacity-60 group-hover:translate-y-1 transition" />
+        </a>
+
+        <a
+          className="bg-white p-4 text-gray-700 flex items-center gap-2 rounded-full
+        outline-none focus:scale-[1.15] hover:scale-[1.15] hover:text-gray-950 active:scale-105 transition-all
+        cursor-pointer border border-black/10"
+          href="https://www.linkedin.com/in/flaviamuntean/"
+          target="_blank"
+        >
+          <BsLinkedin />
+        </a>
+
+        <a
+          className="bg-white p-4 text-gray-700 flex items-center gap-2 rounded-full text-[1.35rem]
+        outline-none focus:scale-[1.15] hover:scale-[1.15] hover:text-gray-950 active:scale-105 transition-all
+        cursor-pointer border border-black/10"
+          href="https://github.com/flaviamuntean"
+          target="_blank"
+        >
+          <FaGithubSquare />
+        </a>
+      </motion.div>
     </section>
   );
 };
